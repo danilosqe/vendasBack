2314bf77c46c8b24bf1282c7f3db5dd22229732e
Danilo Nascimento <danilo.nascimento@unicid.edu.br>
2019-04-15 16:18:35 -0300

    iniciando respositorio

---
+++ src/main/java/com/vendas/VendsasBackApplication.java
@@ -0,0 +1,13 @@
+package com.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendsasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendsasBackApplication.class, args);
+	}
+
+}
